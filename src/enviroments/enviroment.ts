8e1c1fb0cabecea17ea5b8474d525a0de0b4c686
JairCoraje <102072560+JairCoraje@users.noreply.github.com>
2023-11-20 06:51:42 -0500

    upd

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,5 +1,5 @@
 export const enviroment={
     production : false,
-    base : "https://credicarapi-xctq.onrender.com/api/"
+    base : "https://pruebaapi-w6wm.onrender.comapi/"
     //base : "http://18.224.136.242:8080/api/"
 }
